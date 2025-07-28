3f0f91b4967f2280339eabe439a98e0d02106be0
Kozlov Vyacheslav <63507153+r3dmistyle@users.noreply.github.com>
2025-07-28 19:56:57 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,6 @@
-package main
+package mathonline
 
-import (
-	"fmt"
 
-	"yourpackage" // ваш пакет
-)
-
-func main() {
-	if sum := yourpackage.Add(1, 2); sum != 3 {
-		panic(fmt.Sprintf("sum expected to be 3; got %d", sum))
-	}
-
-	fmt.Println("Well done!")
+func Add(a, b int) int {
+	return a + b
 }
